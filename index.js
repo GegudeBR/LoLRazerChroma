e345ddeb84445f9ef10dadf75cfef38043b61343
George <67044516+GegudeBR@users.noreply.github.com>
2022-06-19 03:12:16 -0700

    Update index.js
    Adding a SIGINT catch

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const ChromaSDK = require('./src/ChromaSDK.js');
 const PlayerData = require('./src/PlayerData.js');
 
+const app = {
+  title: 'League of Legends Razer Chroma',
+  description: 'Razer Chroma for League of Legends',
+  author: {
+    name: 'Gegude',
+    contact: 'www.gegude.com'
+  },
+  device_supported: [
+    'keyboard',
+    'mouse'
+  ],
+  category: 'application'
+}
+
+let chroma = new ChromaSDK(app);
+let player = new PlayerData(chroma);
+
 const delay = ms =>
   new Promise((res, rej) => {
     setTimeout(res, ms);
   });
 
 async function start() {
-  const app = {
-    title: 'League of Legends Razer Chroma',
-    description: 'Razer Chroma for League of Legends',
-    author: {
-      name: 'Gegude',
-      contact: 'www.gegude.com'
-    },
-    device_supported: [
-      'keyboard',
-      'mouse'
-    ],
-    category: 'application'
-  }
-
+  
   try {
-    let chroma = new ChromaSDK(app);
+    
     await chroma.connect();
     await delay(1000);
-    let player = new PlayerData(chroma);
     await player.death_animation();
 
     //await chroma.destruct();
@@ -35,5 +38,19 @@ async function start() {
   }
 
 };
+
+async function stop() {
+  await chroma.destruct();
+  process.exit();
+}
+
+process.on('exit', async function () {
+  stop();
+});
+
+
+process.on('SIGINT', async function () {
+  stop();
+});
 
 start();
