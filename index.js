0f19f481f27af7e18daf82dd6c02b67fd96216b5
George <67044516+GegudeBR@users.noreply.github.com>
2022-06-19 18:42:54 -0700

    Update index.js
    Testing new animation class

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const Animation = require('./src/Animation.js');
 const ChromaSDK = require('./src/ChromaSDK.js');
+const Keyboard = require('./src/Keyboard.js');
 const PlayerData = require('./src/PlayerData.js');
 
 const app = {
@@ -16,7 +18,10 @@ const app = {
 }
 
 let chroma = new ChromaSDK(app);
-let player = new PlayerData(chroma);
+let keyboard = new Keyboard(chroma);
+let player = new PlayerData(chroma, keyboard);
+let animation = new Animation(keyboard);
+let AnimationType = Animation.AnimationType;
 
 const delay = ms =>
   new Promise((res, rej) => {
@@ -29,9 +34,23 @@ async function start() {
     
     await chroma.connect();
     await delay(5000);
-    await player.levelup_animation();
-    await delay(2000);
+    animation.add("level_up");
+    await delay(1000);
+
+    animation.add("ally_dragon");
+    //await player.death_animation();
+    //await keyboard.blink(0xFF0000, 25, 5);
+    //keyboard.clear_keyboard();
+    //await player.ally_barron_animation();
+    
+    /*
+    keyboard.clear_keyboard();
+    player.dragon_type = "Fire";
     await player.ally_dragon_animation();
+    
+    player.dragon_type = "Hextech";
+    await player.ally_dragon_animation();
+    */
 
   } catch (err) {
     console.error(err);
