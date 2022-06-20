a63f918b8212fccd8180df78fe17e03764204265
George <67044516+GegudeBR@users.noreply.github.com>
2022-06-19 22:56:18 -0700

    Update Animation.js
    + Fixing level up animation
    + Adding way to call individual dragon animations

--- src/Animation.js
+++ src/Animation.js
@@ -2,9 +2,11 @@ var Enum = require('enum');
 
 const AnimationType = new Enum ({
   ALLY_BARRON: "ally_barron",
-  ALLY_DRAGON: "ally_dragon",
+  ALLY_DRAGON_FIRE: "ally_dragon_fire",
+  ALLY_DRAGON_HEXTECH: "ally_dragon_hextech",
   DEATH: "death",
   LEVEL_UP: "level_up",
+  LOADING: "loading",
 });
 
 const delay = ms =>
@@ -28,8 +30,11 @@ class Animation {
         case AnimationType.ALLY_BARRON.value:
           await this.ally_barron_animation();
           break;
-        case AnimationType.ALLY_DRAGON.value:
+        case AnimationType.ALLY_DRAGON_FIRE.value:
           await this.ally_dragon_animation("Fire");
+          break;
+        case AnimationType.ALLY_DRAGON_HEXTECH.value:
+          await this.ally_dragon_animation("Hextech");
           break;
         case AnimationType.DEATH.value:
           await this.death_animation();
@@ -37,22 +42,12 @@ class Animation {
         case AnimationType.LEVEL_UP.value:
           await this.level_up_animation();
           break;
+        case AnimationType.LOADING.value:
+          await this.loading();
+          break;
       }
       console.log("Animation done");
     }
-  }
-
-  async level_up_animation() {
-    for (var i = 5; i >= 1; i--) {
-      this.keyboard.key[i][1] = 0x01000000 | 0x00ffff;
-      if (i == 5) {
-        this.keyboard.key[i][2] = 0x01000000 | 0x00ffff;
-      }
-      await delay(100);
-      this.keyboard.clear_keyboard();
-    }
-    this.keyboard.clear_keyboard();
-    return;
   }
 
   async ally_barron_animation() {
@@ -96,10 +91,48 @@ class Animation {
     this.keyboard.suspend_update = false;
   }
 
+  async level_up_animation() {
+    for (var i = 5; i >= 1; i--) {
+      this.keyboard.key[i][1] = 0x01000000 | 0x00ffff;
+      if (i == 5) {
+        this.keyboard.key[i][2] = 0x01000000 | 0x00ffff;
+      }
+      await delay(100);
+      if(i == 4) {
+        this.keyboard.key[i+1][2] = 0;
+      } 
+      this.keyboard.key[i][1] = 0;
+    }
+    return;
+  }
+
+  async loading() {
+    this.keyboard.suspend_update = true;
+
+    for (var i = 0; i < 22; i++) {
+      this.keyboard.clear_keyboard();
+      for (var j = 0; j < 6; j++) {
+        this.keyboard.key[j][i] = 0x01000000 | 0xffff00;
+      }
+      await delay(100);
+      this.keyboard.update();
+    }
+    await delay(500);
+    for (var i = 21; i >= 0; i--) {
+      this.keyboard.clear_keyboard();
+      for (var j = 0; j < 6; j++) {
+        this.keyboard.key[j][i] = 0x01000000 | 0xffff00;
+      }
+      await delay(100);
+      this.keyboard.update();
+    }
+    this.keyboard.suspend_update = false;
+    return;
+  }
+
   async add(animation) {
     this.queue.push(animation);
   }
-
 
   async clear() {
     this.queue = [];
